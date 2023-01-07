b860f48239d3b289e4414f1c69d327ec742e5244
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-01-07 23:15:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,4 +20,3 @@ def echo_message(message):
     bot.send_message(message.from_user.id ,text=message1)
     bot.send_message(message.from_user.id ,text=message2)
     bot.send_message(message.from_user.id ,text=message3)
-bot.infinity_polling()
